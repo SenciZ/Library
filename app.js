30e7c36a5fdabd9520ae24224cb7f2b1038b156a
SenciZ <zenkovic88@gmail.com>
2021-04-15 16:37:50 -0500

    Make add button work

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ function Book (title, author, pages, status){
     this.status = status
 }
 
-const addBookButton = document.getElementById("newBookBtn").addEventListener("click", addBookToLibrary);
+const addBookButton = document.getElementById("newBookBtn");
+addBookButton.addEventListener("click", addBookToLibrary);
+
+const titleOfBooka = document.getElementById("title")
+const authorOfBooka = document.getElementById("author")
+const pagesOfBooka = document.getElementById("pages")
+const statusOfBooka = document.getElementById("status")
+
 
 function addBookToLibrary(){
+    const book = new Book(`${titleOfBooka.value}`, `${authorOfBooka.value}`, `${pagesOfBooka.value}`, `${statusOfBooka.value}`)
+    myLibrary.push(book)
     const bookContainer = document.getElementById("bookContainer");    
     const bookDiv = document.createElement("div");
         bookDiv.classList.add("book");
+        bookDiv.innerHTML = titleOfBooka.value
     bookContainer.appendChild(bookDiv);
+    event.preventDefault();
 }
+
 
