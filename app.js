151fe4f23cb3d25a375f3fa4f8d49183dba81008
SenciZ <zenkovic88@gmail.com>
2021-04-15 17:45:50 -0500

    Separate functions

--- app.js
+++ app.js
@@ -19,29 +19,53 @@ const statusOfBook = document.getElementById("status")
 function addBookToLibrary(){
     const book = new Book(titleOfBook.value, authorOfBook.value, pagesOfBook.value, statusOfBook.value)
     myLibrary.push(book)
-    const bookContainer = document.getElementById("bookContainer");    
-    const bookDiv = document.createElement("div");
-    const bookTitleInDiv = document.createElement("h1");
-        bookTitleInDiv.classList.add("titleOfBook");
-        bookTitleInDiv.textContent = titleOfBook.value
-        bookDiv.appendChild(bookTitleInDiv);
+    // const bookContainer = document.getElementById("bookContainer");    
+    // const bookDiv = document.createElement("div");
+    // const bookTitleInDiv = document.createElement("h1");
+    //     bookTitleInDiv.classList.add("titleOfBook");
+    //     bookTitleInDiv.textContent = titleOfBook.value
+    //     bookDiv.appendChild(bookTitleInDiv);
 
-    const bookAuthorInDiv = document.createElement("h2")
-        bookAuthorInDiv.classList.add("authorOfBook");
-        bookAuthorInDiv.textContent = authorOfBook.value
-        bookDiv.appendChild(bookAuthorInDiv);
+    // const bookAuthorInDiv = document.createElement("h2")
+    //     bookAuthorInDiv.classList.add("authorOfBook");
+    //     bookAuthorInDiv.textContent = authorOfBook.value
+    //     bookDiv.appendChild(bookAuthorInDiv);
 
-    const bookPagesInDiv = document.createElement("h2")
-        bookPagesInDiv.classList.add("numberOfPages");    
-        bookPagesInDiv.textContent = pagesOfBook.value
-        bookDiv.appendChild(bookPagesInDiv);
+    // const bookPagesInDiv = document.createElement("h2")
+    //     bookPagesInDiv.classList.add("numberOfPages");    
+    //     bookPagesInDiv.textContent = pagesOfBook.value
+    //     bookDiv.appendChild(bookPagesInDiv);
 
-    bookDiv.classList.add("book");
-    bookContainer.appendChild(bookDiv);
+    // bookDiv.classList.add("book");
+    // bookContainer.appendChild(bookDiv);
+    addAllBooks();
     event.preventDefault();
     titleOfBook.value = "";
     authorOfBook.value = "";
     pagesOfBook.value = "";
 }
 
+function addAllBooks(){
+    myLibrary.forEach(element => {
+        const bookContainer = document.getElementById("bookContainer");    
+        const bookDiv = document.createElement("div");
+        const bookTitleInDiv = document.createElement("h1");
+            bookTitleInDiv.classList.add("titleOfBook");
+            bookTitleInDiv.textContent = titleOfBook.value
+            bookDiv.appendChild(bookTitleInDiv);
+    
+        const bookAuthorInDiv = document.createElement("h2")
+            bookAuthorInDiv.classList.add("authorOfBook");
+            bookAuthorInDiv.textContent = authorOfBook.value
+            bookDiv.appendChild(bookAuthorInDiv);
+    
+        const bookPagesInDiv = document.createElement("h2")
+            bookPagesInDiv.classList.add("numberOfPages");    
+            bookPagesInDiv.textContent = pagesOfBook.value
+            bookDiv.appendChild(bookPagesInDiv);
+    
+        bookDiv.classList.add("book");
+        bookContainer.appendChild(bookDiv);
+    });
+}
 
