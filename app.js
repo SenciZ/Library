95b194437d2a1dd0624f339a70a81da8214fe3ef
SenciZ <zenkovic88@gmail.com>
2021-04-15 16:58:58 -0500

    Create appending element in book div

--- app.js
+++ app.js
@@ -10,21 +10,38 @@ function Book (title, author, pages, status){
 const addBookButton = document.getElementById("newBookBtn");
 addBookButton.addEventListener("click", addBookToLibrary);
 
-const titleOfBooka = document.getElementById("title")
-const authorOfBooka = document.getElementById("author")
-const pagesOfBooka = document.getElementById("pages")
-const statusOfBooka = document.getElementById("status")
+const titleOfBook = document.getElementById("title")
+const authorOfBook = document.getElementById("author")
+const pagesOfBook = document.getElementById("pages")
+const statusOfBook = document.getElementById("status")
 
 
 function addBookToLibrary(){
-    const book = new Book(`${titleOfBooka.value}`, `${authorOfBooka.value}`, `${pagesOfBooka.value}`, `${statusOfBooka.value}`)
+    const book = new Book(titleOfBook.value, authorOfBook.value, pagesOfBook.value, statusOfBook.value)
     myLibrary.push(book)
     const bookContainer = document.getElementById("bookContainer");    
     const bookDiv = document.createElement("div");
-        bookDiv.classList.add("book");
-        bookDiv.innerHTML = titleOfBooka.value
+    const bookTitleInDiv = document.createElement("h1");
+        bookTitleInDiv.classList.add("titleOfBook");
+        bookTitleInDiv.textContent = titleOfBook.value
+        bookDiv.appendChild(bookTitleInDiv);
+
+    const bookAuthorInDiv = document.createElement("h2")
+        bookAuthorInDiv.classList.add("authorOfBook");
+        bookAuthorInDiv.textContent = authorOfBook.value
+        bookDiv.appendChild(bookAuthorInDiv);
+
+    const bookPagesInDiv = document.createElement("h2")
+        bookPagesInDiv.classList.add("numberOfPages");    
+        bookPagesInDiv.textContent = pagesOfBook.value
+        bookDiv.appendChild(bookPagesInDiv);
+
+    bookDiv.classList.add("book");
     bookContainer.appendChild(bookDiv);
     event.preventDefault();
+    titleOfBook.value = "";
+    authorOfBook.value = "";
+    pagesOfBook.value = "";
 }
 
 
